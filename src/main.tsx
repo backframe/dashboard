e89c30a97b7f9197a1aa76cb9bd2056f9734029f
Victor Ndaba <mwangindaba@gmail.com>
2022-10-20 22:18:23 +0300

    feat: started work on the app router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Index from './pages';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 )
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route index element={<Index />} />
+    </Routes>
+  )
+}
