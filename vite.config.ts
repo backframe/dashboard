1ba51bd2c7ac53dac58c1d6327db531ad8308386
Victor Ndaba <mwangindaba@gmail.com>
2022-10-24 22:02:52 +0300

    build: updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/static/",
+  ssr: {
+    format: "cjs",
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
