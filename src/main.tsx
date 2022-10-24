1d1dfc719affcfa1b025916f7f204ef82711f1ac
Victor Ndaba <mwangindaba@gmail.com>
2022-10-24 22:09:02 +0300

    refactor: added react-router base name

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Index from './pages';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Index from "./pages";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/admin">
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
 
 function AppRoutes() {
   return (
     <Routes>
       <Route index element={<Index />} />
     </Routes>
-  )
+  );
 }
