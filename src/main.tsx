cc318a3a7e4d6f30c6747de37ba4cac948e2e98b
Victor Ndaba <mwangindaba@gmail.com>
2022-11-19 22:06:55 +0300

    feat: registered dashboard ui routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Index from "./pages";
+import { NotFound } from "./pages/404";
+import { App } from "./pages/app";
+import { Analytics } from "./pages/app/analytics";
+import { Authentication } from "./pages/app/auth";
+import { Database } from "./pages/app/database";
+import { Overview } from "./pages/app/overview";
+import { Plugins } from "./pages/app/plugins";
+import { Resources } from "./pages/app/resources";
+import { Storage } from "./pages/app/storage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -20,6 +29,16 @@ function AppRoutes() {
         <Route path="login" element={<Index />} />
         <Route path="register" element={<Index />} />
       </Route>
+      <Route path="/app" element={<App />}>
+        <Route path="overview/*" element={<Overview />} />
+        <Route path="analytics/*" element={<Analytics />} />
+        <Route path="auth/*" element={<Authentication />} />
+        <Route path="database/*" element={<Database />} />
+        <Route path="plugins/*" element={<Plugins />} />
+        <Route path="storage/*" element={<Storage />} />
+        <Route path="resources/*" element={<Resources />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
