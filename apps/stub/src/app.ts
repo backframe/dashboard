a0d3429e32a7dd3bc0c469824bb2a95cb49f3de4
Victor Ndaba <mwangindaba@gmail.com>
2022-12-12 23:37:35 +0300

    chore(stub): started work on stub

---
+++ apps/stub/src/app.ts
@@ -0,0 +1,46 @@
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { hasAdmin } from "./db.js";
+import _router from "./routes/index.js";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// middleware
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// mount stub routes
+app.use("/_/api/", _router);
+app.use("/_admin", async (_req, res) => {
+  // check if installer set manually
+  const {
+    headers: { host },
+    protocol,
+    originalUrl: ogUrl,
+  } = _req;
+  const isInitialReq = ogUrl === "/_admin" || ogUrl === "/_admin/";
+
+  if (isInitialReq && !(await hasAdmin())) {
+    return res.redirect(`${protocol}://${host}${ogUrl}?bf_installer#`);
+  }
+
+  if (ogUrl.includes("bf_installer") && (await hasAdmin())) {
+    return res.redirect(`${protocol}://${host}/_admin/login`);
+  }
+
+  res.sendFile(path.join(__dirname, "../../ui/dist/index.html"));
+});
+app.use("/_/static", express.static(path.join(__dirname, "../../ui/dist")));
+
+export default app;
