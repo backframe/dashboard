8b77c3fb4f9de4a0a986217aac9299022e1bf410
Victor Ndaba <mwangindaba@gmail.com>
2022-12-21 21:51:21 +0300

    feat(ui): added react-query provider

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./index.css";
 import { Router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
+
+const client = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={client}>
+      <Router />
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
+}
