1f2eb7fa9391761bb794afcea92f05fd66971504
Victor Ndaba <mwangindaba@gmail.com>
2022-12-21 21:51:55 +0300

    chore(ui): updated index page

--- apps/ui/src/pages/index.tsx
+++ apps/ui/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Navigate } from "react-router-dom";
 import Login from "./login";
 import Register from "./register";
 
@@ -6,6 +7,8 @@ export default function Index() {
   const realQueryParams = new URLSearchParams(window.location.search);
   if (realQueryParams.has("bf_installer")) {
     return <Register />;
+  } else if (localStorage.getItem("bf_admin_auth")) {
+    return <Navigate to="/app" />;
   }
   return <Login />;
 }
